0f2b3432732ca25c28fdc8f08926229f2788af28
sujitmundhe-sdet <sujitmundhe5@gmail.com>
2026-03-14 19:33:14 +0530

    WorkingTests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,18 +1,19 @@
 from playwright.sync_api import Page, expect
 from pages.login_page import LoginPage
-from pages.common import Common
+from pages.common import CommonPage
 
 
 def test_valid_login(page: Page, config):
     login = LoginPage(page)
-    com = Common()
+    com = CommonPage()
+    com.navigate_to_url(page, config['base_url'])
     login.login_to_saucelab(config['username'], config['password'])
     login.verify_login_success()
 
 
 def test_invalid_login(page: Page, config):
     login = LoginPage(page)
-    com = Common()
+    com = CommonPage()
     com.navigate_to_url(page, config['base_url'])
     login.invalid_login_to_saucelab(config['invalid_username'], config['password'])
     login.verify_error_message()
